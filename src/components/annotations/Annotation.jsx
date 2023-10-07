556b1db5b01e4074999f8b811f5e28fe3167dea9
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-10-07 20:18:22 +0200

    Add files via upload

--- src/components/annotations/Annotation.jsx
+++ src/components/annotations/Annotation.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./annotation.css"
 import { Html } from '@react-three/drei'
 
 
 export default function Annotation() {
+  const [hidden, set] = useState()
+
   return (
     <>
-      <Html rotation-y={4.4} position={[-1.25, 0.58, -0.5]} transform occlude>
-        <div className="annotation">
-          <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
-          <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>CODE</a>
-        </div>
-      </Html>
-
-      <Html rotation-y={4.4} position={[-1.25, -0.25, -0.5]} transform occlude>
-        <div className="annotation">
-          <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
-          <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>CODE</a>
-        </div>
-      </Html>
-
-      <Html rotation-y={3.6} position={[-0.55, 0.58, -1.24]} transform occlude>
-        <div className="annotation">
-          <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
-          <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>CODE</a>
-        </div>
-      </Html>
-
-      <Html rotation-y={3.6} position={[-0.55, -0.25, -1.24]}  transform occlude>
-        <div className="annotation">
-          <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
-          <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>CODE</a>
-        </div>
-      </Html>
+      <group position={[-0.8, 0.96, -1.15]}  rotation-y={3.7} >
+        <Html 
+          distanceFactor={3}
+          transform 
+          occlude   
+          castShadow
+          onOcclude={set}
+          style={{
+            transition: 'all 0.5s',
+            opacity: hidden ? 0 : 1,
+            transform: `scale(${hidden ? 0.5 : 1})`
+            }}
+          >
+          <div className="annotation-wrapper">
+            <div className="annotation zleep">
+              <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
+              <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>&lt;/&gt;</a>
+            </div>
+            <div className="annotation sneaker-headz">
+              <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
+              <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>&lt;/&gt;</a>
+            </div>
+            <div className="annotation dough-boyz">
+              <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
+              <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>&lt;/&gt;</a>
+            </div>
+            <div className="annotation coffe-inc">
+              <a className='live' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>LIVE</a>
+              <a className='code' href='https://codesandbox.io/s/qyz5r?file=/src/App.js:1140-1170' target='_blank'>&lt;/&gt;</a>
+            </div>
+          </div>
+        </Html>
+      </group>
     </>
   )
 }
