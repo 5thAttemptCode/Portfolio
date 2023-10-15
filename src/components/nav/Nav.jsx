a81724bc28e9716720fe5f47566c482a9363ce31
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-10-15 20:51:45 +0200

    Add files via upload

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,30 +5,32 @@ import { useConfigurator } from '../../context/Context'
 
 export default function Nav() {
 
-  const { cameraPosition, setCameraPosition } = useConfigurator()
+  const { setCameraPosition } = useConfigurator()
+  const { setModalOpen } = useConfigurator()
   
   return (
     <>
-        <div className="logo">
-            <div className="triangle">
-              <div className="triangle-inner"></div>
-            </div>
-            <div className="name" onClick={() => setCameraPosition([0, 0.1, 9])}>
-              <p>H3NRY</p>
-            </div>
+      <div className="logo nav-item" onClick={() => setCameraPosition([0, 0.1, 9])} >
+        <div className="nav-triangle triangle">
+          <div className="nav-triangle-inner triangle-inner"></div>
         </div>
-        <div className="menu">
-          <li onClick={() => setCameraPosition([-6.88, 0.67, -5.77])}>PROJECTS</li>
-          <li onClick={() => setCameraPosition([7.81, 0.67, -4.43])}>ABOUT</li>
-          <li onClick={() => setCameraPosition([7.78, -0.74, -4.46])}>CONTACT</li>
+        <div className="name">
+          <p>H3NRY</p>
         </div>
-        <div className="info">
-          <a href="#">Mail</a>
-          <a href="https://github.com/5thAttemptCode" target='_blank'>GitHub</a>
-          <a href="https://www.linkedin.com/in/henrik-fuerst-10b58a187/" target='_blank'>LinkedIn</a>
-          <a href="https://codepen.io/your-work/" target='_blank'>Codepen</a>
-        </div>
-       
+      </div>
+
+      <div className="menu nav-item">
+        <p onClick={() => setCameraPosition([-6.61, 2.11, -5.73])}>PROJECTS</p>
+        <p onClick={() => setCameraPosition([7.06, 1.57, -5.31])}>ABOUT</p>
+        <p onClick={() => setCameraPosition([6.89, 0.67, -5.75])}>CONTACT <span>🡣</span></p>
+      </div>
+
+      <div className="info nav-item">
+        <p onClick={() => setModalOpen(true)}>E-mail</p>
+        <a href="https://github.com/5thAttemptCode" target='_blank'>GitHub</a>
+        <a href="https://www.linkedin.com/in/henrik-fuerst-10b58a187/" target='_blank'>LinkedIn</a>
+        <a href="https://codepen.io/your-work/" target='_blank'>Codepen</a>
+      </div>
     </>
   )
 }
