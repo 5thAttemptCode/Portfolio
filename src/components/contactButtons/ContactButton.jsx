5d0cebe3b13199ee3c8e4d9855a0b832ea95e840
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-10-15 20:50:04 +0200

    Add files via upload

--- src/components/contactButtons/ContactButton.jsx
+++ src/components/contactButtons/ContactButton.jsx
@@ -3,76 +3,68 @@ import "./contactButton.css"
 import { Html } from '@react-three/drei'
 import { useConfigurator } from '../../context/Context'
 
+
+function Contact(props){
+  
+  const [ hidden, set ] = useState()
+  const { modalOpen } = useConfigurator()
+
+  return(
+    <Html
+      position={props.position}
+      rotation-y={props.rotation}
+      occlude
+      transform 
+      distanceFactor={3}
+      onOcclude={set}
+      style={{
+        opacity: hidden ? 0 : 1,
+        transform: `scale(${hidden ? 0 : 1})`,
+        display: modalOpen ? "none" : "block",
+        pointerEvents: modalOpen ? "none" : "auto",
+      }}
+      >
+        <div className="contact-wrapper">
+          {props.text}
+        </div>
+    </Html>
+  )
+}
+
+
 export default function ContactButton() {
 
-  const [hidden, set] = useState()
-  const { modalOpen, setModalOpen } = useConfigurator()
+  const { setModalOpen } = useConfigurator()
 
   return (
     <>
-      <Html
+      <Contact 
         position={[1.2, -0.95, -0.7]} 
-        rotation-y={2.1}
-        occlude
-        transform 
-        distanceFactor={3}
-        onOcclude={set}
-        style={{
-          transition: 'all 0.5s',
-          opacity: hidden ? 0 : 1,
-          transform: `scale(${hidden ? 0.5 : 1})`,
-          display: modalOpen ? "none" : "block",
-          pointerEvents: modalOpen ? "none" : "auto",
-          }}
-      >
-        <div onClick={() => setModalOpen(true)} className="contact-wrapper mail">
-          ✉️
-        </div>
-      </Html>
-
-      <Html
+        rotation={2.1}
+        text={
+              <div className='mail' onClick={() => setModalOpen(true)}>
+                ✉️
+              </div>
+             }
+      />
+      <Contact
         position={[0.87, -0.95, -1.09]} 
-        rotation-y={2.45}
-        occlude
-        transform 
-        distanceFactor={3}
-        onOcclude={set}
-        style={{
-          transition: 'all 0.5s',
-          opacity: hidden ? 0 : 1,
-          transform: `scale(${hidden ? 0.5 : 1})`,
-          display: modalOpen ? "none" : "block",
-          pointerEvents: modalOpen ? "none" : "auto",
-          }}
-      >
-        <div className="contact-wrapper">
-          <a href="#">
-            <img src="../../public/Logos/github-black.png" alt="" />
-          </a>
-        </div>
-      </Html>
-
-      <Html
-        position={[0.45, -0.95, -1.357]} 
-        rotation-y={2.68}
-        occlude
-        transform 
-        distanceFactor={3}
-        onOcclude={set}
-        style={{
-          transition: 'all 0.5s',
-          opacity: hidden ? 0 : 1,
-          transform: `scale(${hidden ? 0.5 : 1})`,
-          display: modalOpen ? "none" : "block",
-          pointerEvents: modalOpen ? "none" : "auto",
-          }}
-      >
-        <div className="contact-wrapper">
-          <a href="#">
-            <img src="../../public/Logos/linkedin.png" alt="" />
-          </a>
-        </div>
-      </Html>
+        rotation={2.45}
+        text={
+              <a href="https://github.com/5thAttemptCode" target='_blank'>
+                <img src="../../public/Logos/github-black.png" alt="" />
+              </a>
+             }
+      />
+      <Contact
+        position={[0.45, -0.95, -1.357]}
+        rotation={2.68}
+        text={
+              <a href="https://www.linkedin.com/in/henrik-fuerst-10b58a187/" target='_blank'>
+                <img src="../../public/Logos/linkedin.png" alt="" />
+              </a>
+             }
+      />
     </>
   )
 }
