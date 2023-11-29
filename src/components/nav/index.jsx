b5d6817ac45b86b6b777347f82757703b9d5dc19
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-29 23:55:18 +0100

    Add files via upload

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './style.css'
 
 import { useConfigurator } from '../../context'
@@ -6,12 +6,35 @@ import { useConfigurator } from '../../context'
 
 export default function Nav() {
 
+  //Toggle the menu_________________________________
+  const [ menuOpen, setMenuOpen ] = useState(false)
+  let menuRef = useRef()
+
+  useEffect(() => {
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setMenuOpen(false);
+      }      
+    }
+
+    document.addEventListener("mousedown", handler)
+
+    return() =>{
+      document.removeEventListener("mousedown", handler)
+    }
+  })
+
+  const closeMenu = () => setMenuOpen(false);
+
+  //Toggle the modal_________________________________
   const { setModalOpen } = useConfigurator()
+
+  //Toggle the camera position_________________________________
   const { setCameraPosition } = useConfigurator()
   
   return (
     <>
-      <div className="logo nav-item">
+      <div className="logo nav-item" onClick={closeMenu}>
         <div className="nav-triangle triangle">
           <div className="nav-triangle-inner triangle-inner"></div>
         </div>
@@ -20,17 +43,21 @@ export default function Nav() {
         </div>
       </div>
 
-      <div className="menu nav-item">
-        <p onClick={() => setCameraPosition([-6.61, 2.11, -5.73])}>PROJECTS</p>
-        <p onClick={() => setCameraPosition([7.06, 1.57, -5.31])}>ABOUT</p>
-        <p onClick={() => setCameraPosition([6.89, 0.67, -5.75])}>CONTACT</p>
-      </div>
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}></div>
 
-      <div className="info nav-item">
-        <p onClick={() => setModalOpen(true)}>E-mail</p>
-        <a href="https://github.com/5thAttemptCode" target='_blank'>GitHub</a>
-        <a href="https://www.linkedin.com/in/henrik-fuerst-10b58a187/" target='_blank'>LinkedIn</a>
-        <a href="https://codepen.io/your-work/" target='_blank'>Codepen</a>
+      <div ref={menuRef} className={menuOpen ? "menu-container active" : "menu-container"}>
+        <div className="menu">
+          <p onClick={() => { setCameraPosition([-6.61, 2.11, -5.73]); closeMenu(); }}>PROJECTS</p>
+          <p onClick={() => { setCameraPosition([7.06, 1.57, -5.31]); closeMenu(); }}>ABOUT</p>
+          <p onClick={() => { setCameraPosition([6.89, 0.67, -5.75]); closeMenu(); }}>CONTACT</p>
+        </div>
+        <br />
+        <div className="info">
+          <p onClick={() => setModalOpen(true)}>E-mail</p>
+          <a onClick={() => setMenuOpen(false)} href="https://github.com/5thAttemptCode" target='_blank'>GitHub</a>
+          <a onClick={() => setMenuOpen(false)} href="https://www.linkedin.com/in/henrik-fuerst-10b58a187/" target='_blank'>LinkedIn</a>
+          <a onClick={() => setMenuOpen(false)} href="https://codepen.io/your-work/" target='_blank'>Codepen</a>
+        </div>
       </div>
     </>
   )
