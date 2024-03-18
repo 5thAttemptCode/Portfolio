e9f04608021aa071ea2397bef039bc465273fa62
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-18 22:37:33 +0100

    Improved code readability

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -8,51 +8,52 @@ export default function Modal() {
 
   //Modal
   const { modalOpen, setModalOpen } = useConfigurator()
-  if (!modalOpen) return null
 
   //Email JS
   const form = useRef()
   const [ submitText, setSubmitText ] = useState("SEND MAIL")
-  const [ background, setBackground ] = useState("linear-gradient(to right, #38bdf8, #c084fc)")
+  const [ background, setBackground ] = useState("#000")
 
   function sendEmail(e){
     e.preventDefault()
  
     emailjs.sendForm("service_lvjifwh", "template_kdcdb1e", form.current, "76EmLHF7jqlJMKQqj")
       .then((result) => {
-        setSubmitText("THANK YOU 🤙🏽")
-        setBackground("#6eaa5e")
+        setSubmitText("Thank you!")
+        setBackground("#538c50")
         setTimeout(() => {
           setModalOpen(false)
-        }, 2000)
+          setSubmitText('SEND MAIL')
+          setBackground('#000')
+        }, 1200)
 
       }, (error) => {
         console.log(error.text);
-        setSubmitText("Error 💀")
+        setSubmitText("Error")
         setBackground("#f2003c")
       });
       //Resets form after submit
       e.target.reset()
   }
-  
 
+  if(!modalOpen) return null
+  
   return (
     <div className='overlay'>
-      <div onClick={() => setModalOpen(false)} className="close">
-        <div></div>
-        <div></div>
-      </div>
+      <div className="close" onClick={() => setModalOpen(false)}></div>
       <div className="modal">
         <h1>Get in touch</h1>
         <form ref={form} onSubmit={sendEmail}>
-          <input className='form-input' name="name" type="text" placeholder='your Name' required />
-          <input className='form-input' name="email" type="email" placeholder='your Email' required />
-          <textarea className='form-input' name="message" placeholder='your Message' required />
-          <input className="submit" type="submit" value={submitText} style={{background}} />        
+          <input className='form-input' type="text" placeholder='your Name' required />
+          <input className='form-input' type="email" placeholder='your Email' required />
+          <textarea className='form-input' placeholder='your Message' required />
+          <button className="submit" type="submit" style={{background}}>
+            {submitText}
+          </button>      
         </form>
-        <div>
+        <div className='contact-links'>
           <a href="https://github.com/5thAttemptCode" target='_blank'>GitHub</a>
-          <a href="https://www.linkedin.com/in/henrik-fuerst-10b58a187/" target='_blank'>LinkedIn</a>
+          <a href="https://www.linkedin.com/in/henry-fuerst-10b58a187/" target='_blank'>LinkedIn</a>
         </div>
       </div>
     </div>
