84b042863a0b5f20cac048bac7bc4e3f388b2d6c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-19 12:42:01 +0100

    Project array + contact info array

---
+++ src/components/data/contactData/index.jsx
@@ -0,0 +1,18 @@
+export const ContactData= [
+    {
+        id: 1,
+        contactName: "GitHub",
+        contactURL: "https://github.com/5thAttemptCode",
+        contactIMG: "/Logos/github-black.png",
+        contactPosition: [0.87, -0.95, -1.09],
+        contactRotation: 2.45
+    },
+    {
+        id: 2,
+        contactName: "Linkedin",
+        contactURL: "https://www.linkedin.com/in/henry-fuerst-10b58a187/",
+        contactIMG: "/Logos/linkedin.png",
+        contactPosition: [0.45, -0.95, -1.357],
+        contactRotation: 2.68
+    }
+]
---
+++ src/components/data/projectData/index.jsx
@@ -0,0 +1,30 @@
+export const ProjectData = [
+    {
+        id: 1,
+        projectURL: "https://floor-planer.netlify.app/",
+        projectCode: "https://github.com/5thAttemptCode/FloorPlaner-MERN_R3F_Docker",
+        projectType: "E-Commerce",
+        projectTechStack: "MERN, Docker, R3F, Blender",
+    },
+    {
+        id: 2,
+        projectURL: "https://zleep-cbd.netlify.app/",
+        projectCode: "https://github.com/5thAttemptCode/React_R3F_Full-project_ZLEEP",
+        projectType: "Product Page",
+        projectTechStack: "R3F, Firebase, Paypal, Blender, Inkscape",
+    },
+    {
+        id: 3,
+        projectURL: "https://sneaker-headz.netlify.app",
+        projectCode: "https://github.com/5thAttemptCode/React_Firebase_Full-project_SneakerHeadz",
+        projectType: "E-Commerce",
+        projectTechStack: "React, Firebase, Paypal, Inkscape",
+    },
+    {
+        id: 4,
+        projectURL: "https://doughboyz.netlify.app/",
+        projectCode: "https://github.com/5thAttemptCode/Doughboyz_ThreeJS_Portfolio-project",
+        projectType: "Landing Page",
+        projectTechStack: "Three JS, HTML/CSS",
+    },
+]
