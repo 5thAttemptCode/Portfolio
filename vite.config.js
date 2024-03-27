7a79f62721eadd4765baf71a5caada7d84927c9a
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-27 12:30:25 +0100

    updated config and dev-dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import { VitePluginRadar } from 'vite-plugin-radar'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-              react(),
-              VitePluginRadar({
-                // Google Analytics tag injection
-                analytics: {
-                  id: process.env.VITE_GOOGLE_ANALYTICS_ID
-                },
-              })
-            ],
+            react(),
+            VitePluginRadar({
+              // Google Analytics tag injection
+              analytics: {
+                id: process.env.VITE_GOOGLE_ANALYTICS_ID
+              },
+            })
+          ],
   resolve: {
     alias: {
       '@': '/src',
