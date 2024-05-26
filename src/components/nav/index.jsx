97b7d354142956b35edf23a7b11b28d502eb8a7c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-26 15:18:20 +0200

    context update and new import

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import './style.css'
-import { useConfigurator } from '@/context'
 import ResponsiveMenu from './components/responsiveMenu'
+import { useMenu } from '@/context/menuContext'
+import { useCameraContext } from '@/context/cameraContext'
+import ProjectButton from './components/projectbutton'
 
 
 export default function Nav() {
   
-  const { closeMenu, setCameraPosition } = useConfigurator()
+  const { closeMenu } = useMenu()
+  const { setCameraPosition } = useCameraContext()
   
   return (
     <nav>
-      <div className="logo" onClick={closeMenu}>
-        <p onClick={() => setCameraPosition([-0.04, 1.28, 8.82])}>H3NRY</p>
+      <button 
+        className='cta-logo' 
+        onClick={() => {setCameraPosition([-0.04, 1.28, 8.82]); closeMenu;}}
+      >
+        H<span>3</span>NRY
+      </button>
+      <div className="nav-container">
+        <ProjectButton />
+        <ResponsiveMenu />
       </div>
-      <ResponsiveMenu />
     </nav>
   )
 }
