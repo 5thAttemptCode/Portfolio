c2ee45d4be33556768dcb31a30677b319092591f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-10-14 00:02:16 +1300

    removed <Suspense />, useState for CanvasLoaded

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react'
+import React, { useState } from 'react'
 import { Canvas} from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { LoadingScreen } from '@/components/loadingScreen'
@@ -11,29 +11,38 @@ import DragNotice from '../dragNotice'
 export default function Experience() {
   
   const [ start, setStart ] = useState(false)
+  const [ canvasLoaded, setCanvasLoaded ] = useState(false)
 
   return (
-    <Suspense fallback={null}>
-      <div className="canvas">
-        <LoadingScreen 
-          started={start}  
-          onStarted={() => setStart(true)} 
-        />
+    <div className="canvas">
 
-        <Canvas frameloop="demand" shadows>
-          <OrbitControls 
-            enablePan={false} 
-            enableZoom={false} 
-            minPolarAngle={Math.PI / 2.5} 
-            maxPolarAngle={Math.PI / 2.1} 
-          />
+      <LoadingScreen 
+        started={start}  
+        onStarted={() => setStart(true)} 
+      />
+
+      <Canvas 
+        frameloop="demand" 
+        shadows 
+        onCreated={() => setCanvasLoaded(true)}
+      >
+        <OrbitControls 
+          enablePan={false} 
+          enableZoom={false} 
+          minPolarAngle={Math.PI / 2.5} 
+          maxPolarAngle={Math.PI / 2.1} 
+        />
           {start && <CanExperience />}
-        </Canvas> 
-        
-        <Nav />
-        <Modal />
-        <DragNotice />
-      </div>
-    </Suspense>
+      </Canvas> 
+
+      {canvasLoaded && (
+        <>
+          <Nav />
+          <Modal />
+          <DragNotice />
+        </>
+      )}
+
+    </div>
   )
 }
