47d2a8e97b448b9c8cb1c592f403ab0d682cb230
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-10-13 21:05:55 +1300

    setTimeOut for notice to first appear after 1000ms

--- src/components/dragNotice/index.jsx
+++ src/components/dragNotice/index.jsx
@@ -5,29 +5,42 @@ import { Hand } from 'phosphor-react'
 
 export default function DragNotice() {
 
-  const [ textVisible, setTextVisible ] = useState(true)
+  const [textVisible, setTextVisible] = useState(false)
+
+  // timeOut for DragNotice
+  useEffect(() => {
+    let timeoutId;
+    timeoutId = setTimeout(() => {
+      setTextVisible(true)
+    }, 1200)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  // Remove on first pointerDown
+  const handleClick = () => {
+    setTextVisible(false);
+    window.removeEventListener('pointerdown', handleClick)
+  }
 
   useEffect(() => {
-    const handleClick = () => {
-      setTextVisible(false)
-      window.removeEventListener('pointerdown', handleClick)
-    }
-
     window.addEventListener('pointerdown', handleClick)
 
     return () => {
       window.removeEventListener('pointerdown', handleClick)
     }
-  }, [])
+  }, [ handleClick ])
 
   return (
     <>
-      {textVisible && 
-        <div className='drag-notice'>
+      {textVisible && (
+        <div className='drag-notice' >
           <Hand className='hand' size={32} />
           <h3>Drag to Explore</h3>
         </div>
-      }
+      )}
     </>
-  )
+  );
 }
