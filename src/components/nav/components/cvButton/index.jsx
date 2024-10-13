6ecba6d051ace2c84c6de8829da8108b7cf8e646
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-10-14 00:03:45 +1300

    new class .nav-button, css clean up

--- src/components/nav/components/cvButton/index.jsx
+++ src/components/nav/components/cvButton/index.jsx
@@ -6,7 +6,11 @@ import { FileArrowDown } from 'phosphor-react'
 export default function CvButton() {
   return (
     <abbr title="Download CV">
-      <a className='cv-button' href="/CV/Henry_Fuerst_CV.pdf" target='_blank' >
+      <a 
+        className='cv-button nav-button' 
+        href="/CV/Henry_Fuerst_CV.pdf" 
+        target='_blank' 
+      >
         CV <FileArrowDown size={17} color="#fff" weight="bold" />
       </a>
     </abbr>
