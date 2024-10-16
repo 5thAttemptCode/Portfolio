4cb8f2a13a37f7893f34699176f0eee77f985eef
5thAttemptCode <henrycodes@outlook.com>
2024-10-16 22:00:03 +1300

    useMenu() cleanup and HamburgerMenu import

--- src/components/nav/components/responsiveMenu/index.jsx
+++ src/components/nav/components/responsiveMenu/index.jsx
@@ -5,35 +5,30 @@ import { useMenu } from '@/context/menuContext'
 import { useCameraContext } from '@/context/cameraContext'
 import { useModal } from '@/context/modalContext'
 import { X } from 'phosphor-react'
+import HamburgerMenu from './components/hamburger'
 
 
 export default function ResponsiveMenu() {
 
   const { setCameraPosition } = useCameraContext()
   const { setModalOpen } = useModal()
-  const { 
-    menuOpen, 
-    setMenuOpen, 
-    closeMenu, 
-    menuRef, 
-    hamburgerRef 
-  } = useMenu()
+  const { menuOpen, closeMenu, menuRef } = useMenu()
 
   return (
     <>
-      <abbr title="Open menu">
-        <button 
-          className="hamburger" 
-          ref={hamburgerRef} 
-          onClick={() => setMenuOpen(!menuOpen)} 
-        />
-      </abbr>
-      <div className={menuOpen ? "menu-container active" : "menu-container"} ref={menuRef} >
-        <X size={30} className="close" onClick={() => setMenuOpen(false)} />
+      <HamburgerMenu />
+      <div className={menuOpen ? "menu-container active" : "menu-container"} ref={menuRef}>
+        <X size={30} className="close" onClick={closeMenu} />
         <div className="menu-child menu-links">
-          <p onClick={() => { setCameraPosition([-6.61, 2.11, -5.73]); closeMenu(); }}>Projects</p>
-          <p onClick={() => { setCameraPosition([7.06, 1.57, -5.31]); closeMenu(); }}>About</p>
-          <p onClick={() => { setCameraPosition([6.89, 0.67, -5.75]); closeMenu(); }}>Contact</p>
+          <p onClick={() => { setCameraPosition([-6.61, 2.11, -5.73]); closeMenu(); }}>
+            Projects
+          </p>
+          <p onClick={() => { setCameraPosition([7.06, 1.57, -5.31]); closeMenu(); }}>
+            About
+          </p>
+          <p onClick={() => { setCameraPosition([6.89, 0.67, -5.75]); closeMenu(); }}>
+            Contact
+          </p>
         </div>
         <div className="menu-child quick-contact">
           <h3>Quick contact</h3>
@@ -41,14 +36,20 @@ export default function ResponsiveMenu() {
           {ContactData.map((item) => (
             <a 
               key={item.id}
-              onClick={() => setMenuOpen(false)}
+              onClick={closeMenu}
               href={item.contactURL}
               target='_blank'
             >
               {item.contactName}
             </a>
           ))}
-          <a href="/CV/Henry_Fuerst_CV.pdf" target='_blank'>Download CV</a>
+          <a 
+            href="/CV/Henry_Fuerst_CV.pdf" 
+            onClick={closeMenu} 
+            target='_blank'
+          >
+            Download CV
+          </a>
         </div>
         <span>&copy; by Henry Fuerst</span>
       </div>
