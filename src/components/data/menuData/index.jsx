45f9e044a4b97aaf056c82a9e8f794618892dede
5thAttemptCode <henrycodes@outlook.com>
2024-10-18 20:19:02 +1300

    new data array for menu-links

---
+++ src/components/data/menuData/index.jsx
@@ -0,0 +1,17 @@
+export const MenuData = [
+    {
+        id: 1,
+        name: "Projects",
+        cameraPosition: [-6.61, 1.3, -5.73]
+    },
+    {
+        id: 2,
+        name: "About",
+        cameraPosition: [7.06, 1.57, -5.31]
+    },
+    {
+        id: 3,
+        name: "Contact",
+        cameraPosition: [6.89, 0.67, -5.75]
+    },
+]
