271bebd5c2cbaadd6e0630defea29fa07b352724
5thAttemptCode <henrycodes@outlook.com>
2024-10-19 23:15:02 +1300

    removed useClickOutside from Modal.jsx into ModalContext

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,7 +1,6 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import './style.css'
-import emailjs from '@emailjs/browser'
-import useClickOutside from '../../customHooks/useClickOutside'
+import EmailForm from './components/emailForm'
 import { ContactData } from '@/components/data/contactData'
 import { useModal } from '@/context/modalContext'
 import { X } from 'phosphor-react'
@@ -10,81 +9,38 @@ import { X } from 'phosphor-react'
 export default function Modal() {
 
   // Modal
-  const { modalOpen, closeModal } = useModal()
-
-   // Refs for modal and container
-  const modalRef = useRef()
-  const overlayRef = useRef()
- 
-   // Use the custom hook to handle outside clicks
-  useClickOutside({
-    containerRef: overlayRef, 
-    childRef: modalRef, 
-    isActive: modalOpen, 
-    toggle: closeModal
-  })
-
-  // Email JS
-  const form = useRef()
-  const [ submitText, setSubmitText ] = useState("Submit")
-
-  function sendEmail(e){
-    e.preventDefault()
-
-    setSubmitText("Sending...")
-
-    emailjs.sendForm("service_lvjifwh", "template_kdcdb1e", form.current, "76EmLHF7jqlJMKQqj")
-      .then((result) => {
-        setSubmitText("Success!")
-        setTimeout(() => {
-          e.target.reset() //Resets form after submit
-          closeModal()
-          setSubmitText('Submit')
-        }, 1200)
-
-      }, (error) => {
-        console.log(error.text);
-        setSubmitText("Error..")
-      });
-  }
-
-  if(!modalOpen) return null
+  const { modalOpen, closeModal, overlayRef, modalRef } = useModal()
 
   // Social contact 
   const socialContact = ContactData.slice(0, 2)
 
+  // ensures Modal is rendered only if set to true
+  if(!modalOpen) return null
+
   return (
     <div className='overlay' ref={overlayRef}>
       <div className="modal" ref={modalRef}>
-
         <div className="top-bar">
           <div className="close-bar">
             <X size={20} className="close" onClick={closeModal} />
           </div>
         </div>
-
         <div className="form-wrapper">
           <h2>Drop me a Message</h2>
-          <form ref={form} onSubmit={sendEmail}>
-            <input className='form-input' type="email" placeholder='My email adress is... ' required />
-            <textarea className='form-input' placeholder='I wanted to talk to you about...' required />
-            <button className={submitText === "Sending..." ? "sending" : ""}>
-              {submitText}
-            </button>
-          </form>
+          <EmailForm />
           <div className='contact-links'>
           {socialContact.map((item) => (
             <a 
               key={item.id} 
               href={item.contactURL} 
               target="_blank"
+              onClick={closeModal}
             >
               [ {item.contactName} ]
             </a>
           ))}
           </div>
         </div>
-
       </div>
     </div>
   )
--- src/context/modalContext/index.jsx
+++ src/context/modalContext/index.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from "react"
+import React, { createContext, useContext, useState, useRef } from "react"
+import useClickOutside from "@/customHooks/useClickOutside"
 
 
 const ModalContext = createContext()
@@ -6,11 +7,19 @@ const ModalContext = createContext()
 export const ModalProvider = ({ children }) => {
 
   const [ modalOpen, setModalOpen ] = useState(false)
-
+  const overlayRef = useRef(null)
+  const modalRef = useRef(null)
   const closeModal = () => setModalOpen(false)
 
+  useClickOutside({
+    containerRef: overlayRef, 
+    childRef: modalRef, 
+    isActive: modalOpen, 
+    toggle: closeModal
+  })
+
   return (
-    <ModalContext.Provider value={{ modalOpen, setModalOpen, closeModal }}>
+    <ModalContext.Provider value={{ modalOpen, setModalOpen, closeModal, overlayRef, modalRef }}>
       {children}
     </ModalContext.Provider>
   )
