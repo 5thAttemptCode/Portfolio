48d9d26f8200f93a54df8e69a672c0ae55c3a13c
5thAttemptCode <henrycodes@outlook.com>
2024-10-20 21:36:11 +1300

    new feature: copy my email onclick

---
+++ src/components/modal/components/copyEmailButton/index.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+
+export default function CopyEmail() {
+  
+  const initialText = 'Copy email'
+  const [ buttonText, setButtonText ] = useState(initialText)
+  
+  const handleCopyEmail = () => {
+    const email = 'htlfuerst@gmail.com'
+    navigator.clipboard.writeText(email).then(() => {
+      setButtonText('Copied!')
+      setTimeout(() => {
+        setButtonText(initialText)
+      }, 1500)
+    })
+  }
+
+  return (
+    <abbr title='Copy my email adress'>
+      <button onClick={handleCopyEmail} className='copy-email'>
+        [ {buttonText} ]
+      </button>
+    </abbr>
+  )
+}
