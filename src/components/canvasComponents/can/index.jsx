cf6f48b855209174773bdf4794d924fc844cf774
5thAttemptCode <henrycodes@outlook.com>
2024-11-06 23:17:29 +1300

    updated graphic

--- src/components/canvasComponents/can/index.jsx
+++ src/components/canvasComponents/can/index.jsx
@@ -3,10 +3,12 @@ import { useGLTF } from '@react-three/drei'
 
 
 export function SodaCan(props) {
+
   const { nodes, materials } = useGLTF('/Model/SodaCan.gltf')
+
   return (
     <group {...props} dispose={null}>
-      <group position={[0, 3.471, -0.289]} scale={[-1.324, -2.473, -1.324]}>
+      <group position={[0, 3.471, -0.289]}>
         <mesh geometry={nodes.Cylinder003.geometry} material={materials['Material.001']} />
         <mesh geometry={nodes.Cylinder003_1.geometry} material={materials['Material.002']} />
         <mesh geometry={nodes.Cylinder003_2.geometry} material={materials['Material.003']} />
