2a7441527e59c0fadbe54c00475a8fe999df5ca4
5thAttemptCode <henrycodes@outlook.com>
2024-11-13 23:07:51 +1300

    Removed HtmlToolTipView and adjusted styling

--- src/components/canvasComponents/annotations/components/htmlTooltip/index.jsx
+++ src/components/canvasComponents/annotations/components/htmlTooltip/index.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
-import { Tooltip, tooltipClasses } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import { Tooltip, tooltipClasses } from '@mui/material'
 
 
 const HtmlTooltipText = styled(({ className, ...props }) => 
+
   <Tooltip {...props} classes={{ popper: className }} />)(({ theme }) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: 'rgb(0, 0, 0, 0.8)',
-    color: '#2e70ff',
-    backdropFilter: 'blur(10px)',
-    maxWidth: 210,
-    fontSize: theme.typography.pxToRem(16),
-    padding: '1rem',
-    display: "grid",
-    gap: "0.7rem"
-  }
-}))
+    [`& .${tooltipClasses.tooltip}`]: {
+      backdropFilter: 'blur(10px)',
+      backgroundColor: 'rgb(7, 7, 7, 0.7)',
+      color: '#ffffff',
+      display: "grid",
+      gap: "0.7rem",
+      lineHeight: "1.4",
+      maxWidth: 214,
+      padding: '0.75rem 1rem',
+      fontSize: theme.typography.pxToRem(14)
+    }
+  })
+)
 
-
-const HtmlTooltipView = ({ children }) => {
-  const view = "View source code"
-
-  return (
-    <HtmlTooltipText placement="top-start" title={view}>
-      {children}
-    </HtmlTooltipText>
-  )
-}
-
-export { HtmlTooltipText, HtmlTooltipView }
+export { HtmlTooltipText }
