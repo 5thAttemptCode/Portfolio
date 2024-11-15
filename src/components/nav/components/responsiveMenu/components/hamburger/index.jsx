19b01fc994d069c9d6643d8e4e0426ccb217dc0a
5thAttemptCode <henrycodes@outlook.com>
2024-11-15 14:39:18 +1300

    Changed hmaburgerBtn to phosphore-react icon

--- src/components/nav/components/responsiveMenu/components/hamburger/index.jsx
+++ src/components/nav/components/responsiveMenu/components/hamburger/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.css'
+import { List } from 'phosphor-react'
 import { useMenu } from '@/context/menuContext'
 
 
@@ -8,12 +9,14 @@ export default function HamburgerMenu() {
   const { menuRef, menuOpen, setMenuOpen } = useMenu()
   
   return (
-    <abbr title="Open menu">
+    <div className="hamburger-wrapper">
       <button 
         className="hamburger" 
         ref={menuRef} 
         onClick={() => setMenuOpen(!menuOpen)} 
-      />
-    </abbr>
+      >
+        <List color="var(--light-color-100)" size={30} />
+      </button>
+    </div>
   )
 }
