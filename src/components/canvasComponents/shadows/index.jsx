41c46ad8a0f7abf0f1ead9da64a6ee87e0928ba5
5thAttemptCode <henrycodes@outlook.com>
2024-11-16 21:28:53 +1300

    Removed shadows on mobile

--- src/components/canvasComponents/shadows/index.jsx
+++ src/components/canvasComponents/shadows/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { ContactShadows } from '@react-three/drei'
+import useOnMobile from '@/customHooks/useOnMobile'
 
 
 export default function Shadows() {
 
+  const onMobile = useOnMobile()
+  if (onMobile) return null
+
   return (
     <ContactShadows 
       resolution={256} 
-      position={[0, -2.41, 0]} 
+      position={[0, -2.44, 0]} 
       scale={15}
       blur={0.3} 
-      opacity={0.8}
     />
   )
 }
