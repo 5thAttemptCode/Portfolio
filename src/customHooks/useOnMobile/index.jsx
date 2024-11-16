3e84f281f97173233b4316f9579f628cae899d0b
5thAttemptCode <henrycodes@outlook.com>
2024-11-16 21:30:09 +1300

    Set initial state in useOnMobile to avoid conditional checks on first render

--- src/customHooks/useOnMobile/index.jsx
+++ src/customHooks/useOnMobile/index.jsx
@@ -3,19 +3,14 @@ import { useEffect, useState } from 'react'
 
 export default function useOnMobile() {
 
-  const [ onMobile, setOnMobile ] = useState(null)
+  const isMobile = () => window.innerWidth < 930
+  const [ onMobile, setOnMobile ] = useState(isMobile)
 
   useEffect(() => {
-    const handleResize = () => {
-      setOnMobile(window.innerWidth < 930)
-    }
-    
-    handleResize()
+    const handleResize = () => setOnMobile(isMobile)
+
     window.addEventListener('resize', handleResize)
-    
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
+    return () => window.removeEventListener('resize', handleResize)
   }, [])
 
   return onMobile
