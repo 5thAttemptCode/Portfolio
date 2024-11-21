78ec8f9856fd3984a11bcba40989b2f64cebad56
5thAttemptCode <henrycodes@outlook.com>
2024-11-21 17:29:44 +1300

    new custom hook

---
+++ src/customHooks/useCopyEmail/index.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+export function useCopyEmail( defaultText = 'Copy email', timeout = 1500 ) {
+
+  const [ buttonText, setButtonText ] = useState(defaultText)
+
+  const copyEmail = () => {
+    
+    const email = 'htlfuerst@gmail.com'
+
+    navigator.clipboard.writeText(email).then(() => {
+      setButtonText('Copied!')
+      setTimeout(() => {
+        setButtonText(defaultText)
+      }, timeout)
+    })
+  }
+
+  return { buttonText, copyEmail }
+}
