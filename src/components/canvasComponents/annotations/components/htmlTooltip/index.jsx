5c4eda5440688063529b115851fed14c53830392
5thAttemptCode <henrycodes@outlook.com>
2024-11-21 17:24:00 +1300

    Transition-props, disableInteractive and backgroundcolor-prop

--- src/components/canvasComponents/annotations/components/htmlTooltip/index.jsx
+++ src/components/canvasComponents/annotations/components/htmlTooltip/index.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import Grow from '@mui/material/Grow'
 import { styled } from '@mui/material/styles'
 import { Tooltip, tooltipClasses } from '@mui/material'
 
 
-const HtmlTooltipText = styled(({ className, ...props }) => 
+const HtmlTooltipText = styled(({ className, backgroundColor, ...props }) => 
 
-  <Tooltip {...props} classes={{ popper: className }} />)(({ theme }) => ({
+  <Tooltip 
+    {...props} 
+    classes={{ popper: className }} 
+    disableInteractive 
+    TransitionComponent={Grow} 
+    TransitionProps={{ timeout: 250 }}
+  />)(({ theme, backgroundColor="rgb(7, 7, 7, 0.7)" }) => ({
     [`& .${tooltipClasses.tooltip}`]: {
-      backdropFilter: 'blur(10px)',
-      backgroundColor: 'rgb(7, 7, 7, 0.7)',
-      color: '#ffffff',
+      backdropFilter: "blur(10px)",
+      backgroundColor: backgroundColor,
+      color: "#ffffff",
       display: "grid",
       gap: "0.7rem",
       lineHeight: "1.4",
-      maxWidth: 214,
-      padding: '0.75rem 1rem',
-      fontSize: theme.typography.pxToRem(14)
+      padding: "0.5rem 1rem",
+      fontSize: theme.typography.pxToRem(14),
+      maxWidth: 214
     }
   })
 )
