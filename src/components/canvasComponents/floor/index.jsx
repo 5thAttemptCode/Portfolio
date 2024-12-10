6980d07a8f3d176824ff0c16864ddd9bc04ca3f4
5thAttemptCode <henrycodes@outlook.com>
2024-12-11 12:48:50 +1300

    adjusted floor

--- src/components/canvasComponents/floor/index.jsx
+++ src/components/canvasComponents/floor/index.jsx
@@ -4,45 +4,47 @@ import { Grid, MeshReflectorMaterial, useTexture } from '@react-three/drei'
 
 export default function Floor() {
 
-  const [ roughness, normal] = useTexture(["/Textures/Roughness.jpg", "/Textures/Normal.jpg"])
+  const [ roughness, normal ] = useTexture(["/Textures/Roughness.jpg", "/Textures/Normal.jpg"])
+
+  const size = 1
+  const thickness = 1
 
   return (
     <>
       <Grid 
-        renderOrder={-1} 
-        position={[0, -2.4, 0]} 
-        infiniteGrid 
-        cellSize={1.5} 
-        cellThickness={0.6} 
-        sectionSize={3} 
-        sectionThickness={1.6} 
-        sectionColor="blue"
         fadeDistance={28} 
+        infiniteGrid 
+        position={[ 0, -2.4, 0 ]} 
+        renderOrder={-1} 
+        cellSize={size} 
+        cellThickness={thickness} 
+        sectionSize={size} 
+        sectionThickness={thickness} 
+        sectionColor="blue"
       />
       <mesh 
-        castShadow 
-        receiveShadow 
         position-y={-2.45} 
+        receiveShadow 
         rotation-x={Math.PI / -2}
       >
-        <planeGeometry args={[20, 30]}/>
+        <planeGeometry args={[ 30, 30 ]}/>
         <MeshReflectorMaterial 
+          blur={[ 1000, 400 ]}
+          color={[ 0.001, 0.003, 0.015 ]}
+          debug={0}
+          depthToBlurRatioBias={0.25}
           envMapIntensity={0}
-          color={[0.001, 0.003, 0.015]}
+          maxDepthThreshold={1}
+          mixBlur={30}
+          mixContrast={1}
+          minDepthThreshold={0.9}
+          mirror={0}
+          mixStrength={80}
+          normalMap={normal}
+          reflectorOffset={0.2}
+          resolution={256}
           roughness={0.2}
           roughnessMap={roughness}
-          normalMap={normal}
-          blur={[1000, 400]}
-          mixBlur={30}
-          mixStrength={80}
-          mixContrast={1}
-          resolution={256}
-          mirror={0}
-          minDepthThreshold={0.9}
-          maxDepthThreshold={1}
-          depthToBlurRatioBias={0.25}
-          debug={0}
-          reflectorOffset={0.2}
         />
       </mesh>
     </>
